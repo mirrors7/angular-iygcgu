fb0b953ccebff10c4dacee6a32186d88a229ab87
mirrors7 <64843833@qq.com>
2021-01-22 22:11:59 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-iygcgu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-iygcgu)
---
+++ src/app/products.ts
@@ -0,0 +1,28 @@
+export const products = [
+  {
+    name: "iPhone 12",
+    price: 799,
+    description: " 眼前一亮的全新设计"
+  },
+  {
+    name: "iPhone 12 Mini",
+    price: 699,
+    description: " 解放你的另一只手"
+  },
+  {
+    name: "iPhone 12 Pro",
+    price: 899,
+    description: " 让您更专业处理各种事情"
+  },
+  {
+    name: "iPhone 12 Pro Max",
+    price: 999,
+    description: ""
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
